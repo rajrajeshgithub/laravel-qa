a723e3c017f0821966de0129e5ab5cee96e8fcab
rajesh <rajesh@shahluxe.com>
2020-05-11 02:57:35 +0530

    Build Question form

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -29,7 +29,9 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+
+        return view('question.create', compact('question'));
     }
 
     /**
