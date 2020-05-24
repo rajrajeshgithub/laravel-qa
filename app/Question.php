0f55e59179f01b3f3419de79153de4ace0d183e5
rajesh <rajesh@shahluxe.com>
2020-05-25 01:17:23 +0530

    Securing the application from XSS attacks

--- app/Question.php
+++ app/Question.php
@@ -20,6 +20,11 @@ class Question extends Model
         $this->attributes['slug'] = Str::slug($value);
     }
 
+    public function setBodyAttributes($value)
+    {
+        $this->attributes['body'] = clean($value);
+    }
+
     public function getUrlAttribute()
     {
         return route("questions.show", $this->slug);
@@ -44,7 +49,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return \Parsedown::instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
 
     public function answers()
@@ -76,6 +81,21 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
+    }
+
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+
+    public function excerpt($length)
+    {
+        return \Illuminate\Support\Str::limit(strip_tags($this->bodyHtml()),$length);
+    }
+
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 
 }
