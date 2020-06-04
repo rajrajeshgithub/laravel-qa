15593eaf109ac70063ee3b1ea40b699fa0b6250b
rajesh <rajesh@shahluxe.com>
2020-06-05 01:35:53 +0530

    Create answers component and implement vue list rendering

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -1,3 +1,36 @@
+<template>
+    <div class="media post">
+        <vote :model="answer" name="answer"></vote>
+        <div class="media-body">
+            <form v-if="editing" @submit.prevent="update">
+                @csrf
+                <div class="form-group">
+                    <textarea class="form-control" rows="10" v-model="body" required></textarea>
+                </div>
+                <button class="btn btn-primary" :disabled="isInvalid">Update</button>
+                <button class="btn btn-outline-primary" @click="cancel">Cancel</button>
+            </form>
+            <div v-else>
+                <div v-html="bodyHtml"></div>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="ml-auto">
+                            <a v-if="authorize('modify', answer)" @click.prevent="edit" class="btn btn-outline-info btn-sm">Edit</a>
+                            <button v-if="authorize('modify', answer)" @click="destroy" class="btn btn-outline-danger btn-sm" >Delete</button>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                    </div>
+                    <div class="col-4 float-right">
+                        <user-info v-bind:model="answer" label="Answered"></user-info>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
 <script>
     export default {
         props:['answer'],
