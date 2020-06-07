097c734cc55d81228dfcf31945a8ef6b656a2d62
rajesh <rajesh@shahluxe.com>
2020-06-08 03:36:04 +0530

    Implement autosize textarea

--- resources/js/components/REditor.vue
+++ resources/js/components/REditor.vue
@@ -20,6 +20,8 @@
 </template>
 <script>
 import MarkdownIt from "markdown-it";
+import autosize from "autosize";
+
 const md = new MarkdownIt();
 export default {
     props:['body'],
@@ -28,6 +30,19 @@ export default {
         preview(){
             return md.render(this.body);
         }
+    },
+
+   /* watch:{
+        body: function () {
+            console.log('watch body');
+        }
+    },*/
+
+    mounted(){
+        autosize(this.$el.querySelector('textarea'))
+    },
+    updated() {
+        autosize(this.$el.querySelector('textarea'))
     }
 }
 </script>
